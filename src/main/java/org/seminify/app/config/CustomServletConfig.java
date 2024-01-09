e8c3d92e198ae6aaff3ceb9375bfc3af396ea973
seminify <seminify@gmail.com>
2024-01-09 21:56:36 +0900

    REST관련 툴을 이용한 POST/PUT/DELETE

---
+++ src/main/java/org/seminify/app/config/CustomServletConfig.java
@@ -0,0 +1,14 @@
+package org.seminify.app.config;
+
+import org.seminify.app.controller.formatter.LocalDateFormatter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CustomServletConfig implements WebMvcConfigurer {
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addFormatter(new LocalDateFormatter());
+    }
+}
--- src/main/java/org/seminify/app/controller/TodoController.java
+++ src/main/java/org/seminify/app/controller/TodoController.java
@@ -1,11 +1,17 @@
 package org.seminify.app.controller;
 
+import java.util.Map;
+
 import org.seminify.app.dto.PageRequestDTO;
 import org.seminify.app.dto.PageResponseDTO;
 import org.seminify.app.dto.TodoDTO;
 import org.seminify.app.service.TodoService;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +25,13 @@ import lombok.extern.log4j.Log4j2;
 public class TodoController {
     private final TodoService todoService;
 
+    @PostMapping
+    public Map<String, Long> register(@RequestBody TodoDTO todoDTO) {
+        log.info("TodoDTO : " + todoDTO);
+        var tno = todoService.register(todoDTO);
+        return Map.of("TNO", tno);
+    }
+
     @GetMapping("list")
     public PageResponseDTO<TodoDTO> list(PageRequestDTO pageRequestDTO) {
         log.info(pageRequestDTO);
@@ -28,5 +41,20 @@ public class TodoController {
     @GetMapping("{tno}")
     public TodoDTO get(@PathVariable(name = "tno") Long tno) {
         return todoService.get(tno);
+    }
+
+    @PutMapping("{tno}")
+    public Map<String, String> modify(@PathVariable(name = "tno") Long tno, @RequestBody TodoDTO todoDTO) {
+        todoDTO.setTno(tno);
+        log.info("TodoDTO : " + todoDTO);
+        todoService.modify(todoDTO);
+        return Map.of("RESULT", "SUCCESS");
+    }
+
+    @DeleteMapping("{tno}")
+    public Map<String, String> remove(@PathVariable(name = "tno") Long tno) {
+        log.info("TNO : " + tno);
+        todoService.remove(tno);
+        return Map.of("RESULT", "SUCCESS");
     }
 }
