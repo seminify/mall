a6c07d5711ce864d109cedbac09a8e81bef75997
seminify <seminify@gmail.com>
2024-01-09 21:02:51 +0900

    프로젝트 설정

---
+++ src/main/java/org/seminify/app/repository/TodoRepository.java
@@ -0,0 +1,9 @@
+package org.seminify.app.repository;
+
+import org.seminify.app.domain.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoRepository extends JpaRepository<Todo, Long> {
+}
