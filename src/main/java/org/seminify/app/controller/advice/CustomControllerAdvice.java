f905074c8aedf387bb76c2a9860b39d510a307b9
seminify <seminify@gmail.com>
2024-01-09 21:47:45 +0900

    @RestControllerAdvice

---
+++ src/main/java/org/seminify/app/controller/advice/CustomControllerAdvice.java
@@ -0,0 +1,25 @@
+package org.seminify.app.controller.advice;
+
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class CustomControllerAdvice {
+    @ExceptionHandler(NoSuchElementException.class)
+    protected ResponseEntity<?> notFound(NoSuchElementException noSuchElementException) {
+        var message = noSuchElementException.getMessage();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("MESSAGE", message));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<?> notAcceptable(MethodArgumentNotValidException methodArgumentNotValidException) {
+        var message = methodArgumentNotValidException.getMessage();
+        return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(Map.of("MESSAGE", message));
+    }
+}
