2c5a33d5457fcc8dfdead55d3c8da26b08ac982e
seminify <seminify@gmail.com>
2024-01-09 20:05:33 +0900

    VSCode 설치

--- app/src/App.js
+++ app/src/App.js
@@ -1,27 +1,5 @@
-import 'App.css';
-import logo from 'logo.svg';
-
 const App = () => (
-  <div className='App'>
-    <header className='App-header'>
-      <img
-        className='App-logo'
-        src={logo}
-        alt='logo'
-      />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a
-        className='App-link'
-        href='https://reactjs.org'
-        target='_blank'
-        rel='noopener noreferrer'
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
+  <h1 className='text-3xl font-bold underline'>Hello world!</h1>
 );
 
 export default App;
