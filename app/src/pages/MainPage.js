0c308a1e88380c866e1b7b1c10468a995686b15e
seminify <seminify@gmail.com>
2024-01-09 20:20:40 +0900

    <Link>를 통한 이동

--- app/src/pages/MainPage.js
+++ app/src/pages/MainPage.js
@@ -1,3 +1,12 @@
-const MainPage = () => <div className='text-3xl'>Main Page</div>;
+import { Link } from 'react-router-dom';
+
+const MainPage = () => (
+  <div>
+    <div className='flex'>
+      <Link to='/about'>About</Link>
+    </div>
+    <div className='text-3xl'>Main Page</div>
+  </div>
+);
 
 export default MainPage;
