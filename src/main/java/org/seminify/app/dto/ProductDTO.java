9f8e954e5a73d031b5f8dccd956b9472ec77d7a7
seminify <seminify@gmail.com>
2024-01-10 13:46:37 +0900

    파일 업로드를 위한 설정

---
+++ src/main/java/org/seminify/app/dto/ProductDTO.java
@@ -0,0 +1,27 @@
+package org.seminify.app.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Builder
+@Data
+@NoArgsConstructor
+public class ProductDTO {
+    private Long pno;
+    private String pname;
+    private int price;
+    private String pdesc;
+    private boolean delFlag;
+    @Builder.Default
+    private List<MultipartFile> files = new ArrayList<>();
+    @Builder.Default
+    private List<String> uploadFileNames = new ArrayList<>();
+}
